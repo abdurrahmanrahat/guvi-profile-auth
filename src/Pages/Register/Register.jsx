4f5986aabec90c9abbc104aa56d9de42588078dd
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2023-11-06 15:18:27 +0600

    react hook form in register page

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,7 +1,17 @@
+import { useForm } from "react-hook-form";
 import FormImg from "../../assets/login-image.png";
 import { Link } from "react-router-dom";
 
 const Register = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="md:flex items-center gap-4 my-8 md:my-0 lg:p-12">
       {/* image */}
@@ -10,13 +20,10 @@ const Register = () => {
       </div>
       {/* form fields */}
       <div className="md:w-1/2 mx-auto w-full p-4 md:p-8">
-        <form className="p-3 md:p-[2px]">
-          <div className="flex items-center justify-center mb-6">
-            <h2 className="text-[48px] items-center font-[700]">
-              Register User
-            </h2>
-          </div>
-
+        <div className="flex items-center justify-center mb-6">
+          <h2 className="text-[48px] items-center font-[700]">Register User</h2>
+        </div>
+        <form onSubmit={handleSubmit(onSubmit)} className="p-3 md:p-[2px]">
           <div className="flex flex-col gap-y-3">
             {/* Name */}
             <div>
@@ -25,6 +32,7 @@ const Register = () => {
                 className="w-full p-2 border border-solid border-[#ccc] rounded-lg"
                 type="text"
                 placeholder="name"
+                {...register("name", { required: true })}
               ></input>
             </div>
 
@@ -33,8 +41,9 @@ const Register = () => {
               <label className="block mb-[5px] font-bold">Your Email</label>
               <input
                 className="w-full p-2 border border-solid border-[#ccc] rounded-lg"
-                type="password"
-                placeholder="password"
+                type="email"
+                placeholder="email"
+                {...register("email", { required: true })}
               ></input>
             </div>
 
@@ -45,6 +54,7 @@ const Register = () => {
                 className="w-full p-2 border border-solid border-[#ccc] rounded-lg"
                 type="password"
                 placeholder="Enter Password"
+                {...register("password", { required: true })}
               ></input>
             </div>
 
@@ -57,6 +67,7 @@ const Register = () => {
                 className="w-full p-2 border border-solid border-[#ccc] rounded-lg"
                 type="password"
                 placeholder="Confirm Password"
+                {...register("confirmPassword", { required: true })}
               ></input>
             </div>
           </div>
@@ -69,18 +80,18 @@ const Register = () => {
               Register
             </button>
           </div>
-
-          <div className="flex items-center justify-center mt-4">
-            <span className="text-[18px] md:text-[16px] lg:text-[18px]">
-              Already have an account?{" "}
-              <Link to="/login">
-                <span className="text-[#f78719] text-[18px] md:text-[16px] lg:text-[18px] underline cursor-pointer font-[700]">
-                  Login
-                </span>
-              </Link>
-            </span>
-          </div>
         </form>
+
+        <div className="flex items-center justify-center mt-4">
+          <span className="text-[18px] md:text-[16px] lg:text-[18px]">
+            Already have an account?{" "}
+            <Link to="/login">
+              <span className="text-[#f78719] text-[18px] md:text-[16px] lg:text-[18px] underline cursor-pointer font-[700]">
+                Login
+              </span>
+            </Link>
+          </span>
+        </div>
       </div>
     </div>
   );
