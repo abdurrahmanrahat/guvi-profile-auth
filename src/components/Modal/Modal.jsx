f50f86012c809aa2b55dfa7182a05ed082395e3b
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2023-11-07 16:07:17 +0600

    form in modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,10 +1,26 @@
 import { Dialog, Transition } from "@headlessui/react";
-import { Fragment } from "react";
+import { Fragment, useContext } from "react";
+import { AuthContext } from "../../provider/AuthProvider";
+import { useForm } from "react-hook-form";
 
 export default function Modal({ isOpen, setIsOpen }) {
+  const { user } = useContext(AuthContext);
   function closeModal() {
     setIsOpen(false);
   }
+
+  const { register, handleSubmit, reset } = useForm();
+
+  const onCancel = () => {
+    reset();
+    setIsOpen(false);
+  };
+
+  const onSubmit = (data) => {
+    onCancel();
+    console.log(data);
+    const { name, email, age, gender, dob, mobile } = data;
+  };
 
   return (
     <>
@@ -38,24 +54,103 @@ export default function Modal({ isOpen, setIsOpen }) {
                     as="h3"
                     className="text-lg font-medium leading-6 text-gray-900"
                   >
-                    Payment successful
-                  </Dialog.Title>
-                  <div className="mt-2">
-                    <p className="text-sm text-gray-500">
-                      Your payment has been successfully submitted. We’ve sent
-                      you an email with all of the details of your order.
-                    </p>
-                  </div>
+                    {/* form for update */}
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                      <div className="grid grid-cols-1 gap-6">
+                        {/* Name */}
+                        <div>
+                          <label className="block mb-[5px] font-bold">
+                            Your Name
+                          </label>
+                          <input
+                            className="w-full p-2 border border-solid border-[#ccc] rounded-lg"
+                            type="text"
+                            placeholder="name"
+                            defaultValue={user?.displayName}
+                            {...register("name", { required: true })}
+                          ></input>
+                        </div>
 
-                  <div className="mt-4">
-                    <button
-                      type="button"
-                      className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                      onClick={closeModal}
-                    >
-                      Got it, thanks!
-                    </button>
-                  </div>
+                        {/* email */}
+                        <div>
+                          <label className="block mb-[5px] font-bold">
+                            Your Email
+                          </label>
+                          <input
+                            className="w-full p-2 border border-solid border-[#ccc] rounded-lg"
+                            type="email"
+                            placeholder="email"
+                            defaultValue={user?.email}
+                            {...register("email", { required: true })}
+                          ></input>
+                        </div>
+
+                        {/* age */}
+                        <div>
+                          <label className="block mb-[5px] font-bold">
+                            Your Age
+                          </label>
+                          <input
+                            className="w-full p-2 border border-solid border-[#ccc] rounded-lg"
+                            type="number"
+                            placeholder="age"
+                            {...register("age", { required: true })}
+                          ></input>
+                        </div>
+
+                        {/* gender */}
+                        <div className="form-control">
+                          <label className="block mb-[5px] font-bold">
+                            Gender
+                          </label>
+                          <select
+                            className="w-full p-2 border border-solid border-[#ccc] rounded-lg"
+                            {...register("gender", { required: true })}
+                          >
+                            <option>Male</option>
+                            <option>Female</option>
+                            <option>Others</option>
+                          </select>
+                        </div>
+
+                        {/* Date Of Birth */}
+                        <div className="w-full">
+                          <label className="block mb-[5px] font-bold">
+                            DOB
+                          </label>
+                          <input
+                            type="date"
+                            name="dob"
+                            placeholder="-/-/-"
+                            className="w-full p-2 border border-solid border-[#ccc] rounded-lg"
+                            {...register("dob", { required: true })}
+                          />
+                        </div>
+
+                        {/* mobile */}
+                        <div>
+                          <label className="block mb-[5px] font-bold">
+                            Your Mobile
+                          </label>
+                          <input
+                            className="w-full p-2 border border-solid border-[#ccc] rounded-lg"
+                            type="number"
+                            placeholder="number"
+                            {...register("mobile", { required: true })}
+                          ></input>
+                        </div>
+                      </div>
+
+                      <div className="flex items-center justify-center mt-10">
+                        <button
+                          type="submit"
+                          className="w-[60%] bg-[#1575a7] py-2 text-[18px] text-white font-[500] rounded-lg"
+                        >
+                          Updated
+                        </button>
+                      </div>
+                    </form>
+                  </Dialog.Title>
                 </Dialog.Panel>
               </Transition.Child>
             </div>
